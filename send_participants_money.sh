ecbc8699e15e95798a5efaa0a0931dc8e908f989
Thomas Taylor-Hall <tjt12@ubu30gb-templ.doc.ic.ac.uk>
2016-06-17 13:08:24 +0100

    Script to automatically send all participants 5 coins

--- send_participants_money.sh
+++ send_participants_money.sh
@@ -1,1 +1,27 @@
-//TODO: generate blocks, send the addresses money for each participant
+#TODO: generate blocks, send the addresses money for each participant
+
+cd seed_nodes/seed-daemon-1
+
+PARTICIPANTSLOC=/home/tjt12/lotteryapp/participants
+
+count=0
+for D in $PARTICIPANTSLOC/*; do
+  if [ -d "${D}" ]; then
+    count=$count+1
+  fi
+done
+
+coinsNeeded=$((${count}*5))
+blocksNeeded=$((${coinsNeeded} % 50))
+./bitcoin_command.sh generate $((${blocksNeeded} + 101))
+
+
+for D in $PARTICIPANTSLOC/*; do
+  if [ -d "${D}" ]; then
+    ./bitcoin_command.sh sendtoaddress `cat ${D}/address.txt` 5
+  fi
+done
+
+sleep 1
+./bitcoin_command.sh generate 1
+cd -
